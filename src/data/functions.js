97f8de2dbc1d9fbc223a0021d573ab4f17e6d08c
Onur Altuntas <oaltuntas@bimser.com.tr>
2024-08-01 15:53:43 +0300

    type parametresi eklendi

--- src/data/functions.js
+++ src/data/functions.js
@@ -7,22 +7,27 @@ export const data = [
 			{
 				title: 'Work Order',
 				key: '0-0-0',
+				type: 'grid',
 				children: [
 					{
 						title: 'Statuses',
-						key: '0-0-1-0'
+						key: '0-0-1-0',
+						type: 'status'
 					},
 					{
 						title: 'Queries',
-						key: '0-0-2-0'
+						key: '0-0-2-0',
+						type: 'query'
 					},
 					{
 						title: 'Detail Page',
 						key: '0-0-3-0',
+						type: 'detail',
 						children: [
 							{
 								title: 'Rules',
-								key: '0-0-3-1'
+								key: '0-0-3-1',
+								type: 'rule'
 							}
 						]
 					}
@@ -31,22 +36,27 @@ export const data = [
 			{
 				title: 'Work Request',
 				key: '0-0-2',
+				type: 'grid',
 				children: [
 					{
 						title: 'Statuses',
-						key: '0-0-2-0'
+						key: '0-0-2-0',
+						type: 'status'
 					},
 					{
 						title: 'Queries',
-						key: '0-0-2-1'
+						key: '0-0-2-1',
+						type: 'query'
 					},
 					{
 						title: 'Detail Page',
 						key: '0-0-2-3',
+						type: 'detail',
 						children: [
 							{
 								title: 'Rules',
-								key: '0-0-2-4'
+								key: '0-0-2-4',
+								type: 'rule'
 							}
 						]
 					}
