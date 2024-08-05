1547a58a5b653d0864f522a8a923d3abc4c40671
Onur Altuntas <oaltuntas@bimser.com.tr>
2024-08-05 13:40:52 +0300

    update initialColumns for detail page props

--- src/data/queries.js
+++ src/data/queries.js
@@ -1,14 +1,47 @@
 export const initialColumns = [
-	{ id: '1', header: 'Code', name: 'code', visible: true, isPrimaryKey: false, order: 1 },
-	{ id: '2', header: 'Name', name: 'name', visible: true, isPrimaryKey: false, order: 2 },
+	{
+		id: '1',
+		header: 'Code',
+		name: 'code',
+		visible: true,
+		isPrimaryKey: false,
+		order: 1,
+		valueType: 'text'
+	},
+	{
+		id: '2',
+		header: 'Name',
+		name: 'name',
+		visible: true,
+		isPrimaryKey: false,
+		order: 2,
+		valueType: 'text'
+	},
 	{
 		id: '3',
 		header: 'Department',
 		name: 'department',
 		visible: true,
 		isPrimaryKey: false,
-		order: 3
+		order: 3,
+		valueType: 'select'
 	},
-	{ id: '4', header: 'Id', name: 'id', visible: false, isPrimaryKey: true, order: 4 },
-	{ id: '5', header: 'Provider', name: 'provider', visible: false, isPrimaryKey: false, order: 5 }
+	{
+		id: '4',
+		header: 'Id',
+		name: 'id',
+		visible: false,
+		isPrimaryKey: true,
+		order: 4,
+		valueType: 'number'
+	},
+	{
+		id: '5',
+		header: 'Provider',
+		name: 'provider',
+		visible: false,
+		isPrimaryKey: false,
+		order: 5,
+		valueType: 'select'
+	}
 ];
