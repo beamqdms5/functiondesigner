661deaf25c21e2bb5a740421f4db6c8fb7c7b03b
SamiAltuncu <samialtuncu@gmail.com>
2024-08-05 09:46:51 +0300

    query style fix

--- src/components/query/index.js
+++ src/components/query/index.js
@@ -1,32 +1,39 @@
-import React, { useState } from 'react';
 import CustomDndTables from '@/components/customdndtables';
-import { Button } from 'antd';
 import { initialColumns } from '@/data/queries';
+import { Button } from 'antd';
+import { useState } from 'react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 
 const Query = () => {
-    const [columns, setColumns] = useState(initialColumns);
+	const [columns, setColumns] = useState(initialColumns);
 
-    // her bir şablonun kendi datasını güncelle
-    const handleColumnsChange = (updatedColumns) => {
-        setColumns(updatedColumns);
-    };
+	// her bir şablonun kendi datasını güncelle
+	const handleColumnsChange = updatedColumns => {
+		setColumns(updatedColumns);
+	};
 
-    const handlePrintColumns = () => {
-        console.log(columns);
-    };
+	const handlePrintColumns = () => {
+		console.log(columns);
+	};
 
-    return (
-        <DndProvider backend={HTML5Backend}>
-            <div style={{ display: 'flex', gap: '50px', flexDirection: 'column' }}>
-                <CustomDndTables initialColumns={initialColumns} onColumnsChange={handleColumnsChange} />
-                <CustomDndTables initialColumns={initialColumns} onColumnsChange={handleColumnsChange} />
-                <Button onClick={handlePrintColumns}>Print Columns to Console</Button>
-            </div>
-        </DndProvider>
-
-    );
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<div style={{ display: 'flex', gap: '50px', flexDirection: 'column' }}>
+				<CustomDndTables
+					initialColumns={initialColumns}
+					onColumnsChange={handleColumnsChange}
+				/>
+				<CustomDndTables
+					initialColumns={initialColumns}
+					onColumnsChange={handleColumnsChange}
+				/>
+				<div className="d-flex align-items justify-content">
+					<Button onClick={handlePrintColumns}>Print Columns to Console</Button>
+				</div>
+			</div>
+		</DndProvider>
+	);
 };
 
 export default Query;
