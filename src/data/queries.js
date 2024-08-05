275d64f746fc19e2c1574b2c9e12bba52c1ab8f9
Onur Altuntas <oaltuntas@bimser.com.tr>
2024-08-05 10:20:09 +0300

    update initialColumns

--- src/data/queries.js
+++ src/data/queries.js
@@ -1,7 +1,7 @@
 export const initialColumns = [
-	{ id: '1', name: 'Code', visible: true },
-	{ id: '2', name: 'Name', visible: true },
-	{ id: '3', name: 'Department', visible: true },
-	{ id: '4', name: 'Id', visible: false, isPrimaryKey: true },
-	{ id: '5', name: 'Provider', visible: false }
+	{ id: '1', name: 'code', header: 'Code', visible: true, order: 1 },
+	{ id: '2', name: 'name', header: 'Name', visible: true, order: 2 },
+	{ id: '3', name: 'department', header: 'Department', visible: true, order: 3 },
+	{ id: '4', name: 'id', header: 'Id', visible: false, isPrimaryKey: true, order: 4 },
+	{ id: '5', name: 'provider', header: 'Provider', visible: false, order: 5 }
 ];
