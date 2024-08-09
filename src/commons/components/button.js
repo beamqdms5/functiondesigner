3fbc5e614c5bb0e09442eb0e875f251628f3c137
Onur Altuntas <oaltuntas@bimser.com.tr>
2024-08-09 09:59:14 +0300

    btn children error fix

--- src/commons/components/button.js
+++ src/commons/components/button.js
@@ -1,7 +1,7 @@
 import { Button } from 'antd';
 
 const BCButton = ({ ...props }) => {
-	return <Button {...props}>{children}</Button>;
+	return <Button {...props}>{props.children}</Button>;
 };
 
 export default BCButton;
